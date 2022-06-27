69e5db9693df20017d47342d183cbc76d36810ed
Kiselev Vladislav <kiselev.vladislav@urfu.me>
2022-06-27 12:36:03 +0500

    Task 2.1 - Complete

--- Домашняя работа/Part 1. Password Generator/main.cpp
+++ Домашняя работа/Part 1. Password Generator/main.cpp
@@ -34,13 +34,13 @@ std::vector <char> generatePassword(int call_number){
     return password;
 }
 
-int main() {
+void Generator(){
     std::ofstream passwords_file;
 
     fillingTheAlphabet();
 
     std::cout << "Welcome to a password generator!" << std::endl
-    << "Please, enter the desired number of generated passwords: ";
+              << "Please, enter the desired number of generated passwords: ";
     std::cin >> number_of_passwords;
 
     std::cout << "Please, enter the desired password length: ";
@@ -49,23 +49,133 @@ int main() {
     std::cout << "Please, enter the desired filename with the generated passwords: ";
     std::cin >> file_name;
 
-    file_name += ".pass";
+    passwords_file.open(file_name + ".pass");
 
-    passwords_file.open(file_name);
     if(!passwords_file.is_open()){
         std::cout << "ERROR: The program was unable to create an output file for passwords." << std::endl
-        << "The program ended with an error";
-        return 0;
+                  << "The program ended with an error";
+        return;
     }
 
-    for (int i = 1; i < number_of_passwords + 1; ++i) {
+    for (int i = 1; i <= number_of_passwords; ++i) {
         for (auto symbol:generatePassword(i)) {
             passwords_file << symbol;
         }
-        passwords_file << std::endl;
+        passwords_file << " ";
     }
 
-    std::cout << "File " << file_name  << " successfully generated.";
+    std::cout << "File " << file_name + ".pass"  << " successfully generated." << std::endl;
+
+    passwords_file.close();
+}
+
+void Encryptor(){
+    int encryptionKey;
+
+    std::ifstream passwords_file;
+    std::ofstream encrypted_passwords_file;
+
+    passwords_file.open(file_name + ".pass");
+
+    if(!passwords_file.is_open()){
+        std::cout << "ERROR: The program was unable to create an output file for passwords." << std::endl
+                  << "The program ended with an error";
+        return;
+    }
+
+    encrypted_passwords_file.open(file_name + ".encpass");
+
+    if(!encrypted_passwords_file.is_open()){
+        std::cout << "ERROR: The program was unable to create an output file for passwords." << std::endl
+                  << "The program ended with an error";
+        return;
+    }
+
+    std::cout << "Enter number - encryption key: ";
+    std::cin >> encryptionKey;
+
+    while (!passwords_file.eof()){
+        char buffer = (char) passwords_file.get();
+        encrypted_passwords_file << ((char) ((int) buffer + (encryptionKey % 127)));
+    }
+
+    passwords_file.close();
+    encrypted_passwords_file.close();
+
+    std::string remove_file_name = file_name + ".pass";
+    remove(remove_file_name.c_str());
+
+    std::cout << "Complete.";
+}
+
+void Decrypter(){
+    int decryptionKey;
+
+    std::ifstream encrypted_passwords_file;
+    std::ofstream passwords_file;
+
+    std::cout << "Enter a filename without extension: ";
+    std::cin >> file_name;
+
+    encrypted_passwords_file.open(file_name + ".encpass");
+
+    if(!encrypted_passwords_file.is_open()){
+        std::cout << "ERROR: The program was unable to create an output file for passwords." << std::endl
+                  << "The program ended with an error";
+        return;
+    }
+
+    passwords_file.open(file_name + ".pass");
+
+    if(!passwords_file.is_open()){
+        std::cout << "ERROR: The program was unable to create an output file for passwords." << std::endl
+                  << "The program ended with an error";
+        return;
+    }
+
+    std::cout << "Enter number - decryption key: ";
+    std::cin >> decryptionKey;
+
+    while (!encrypted_passwords_file.eof()){
+        char buffer = (char) encrypted_passwords_file.get();
+        passwords_file << ((char) ((int) buffer - (decryptionKey % 127)));
+    }
+
+    passwords_file.close();
+    encrypted_passwords_file.close();
+
+    std::string remove_file_name = file_name + ".encpass";
+    remove(remove_file_name.c_str());
+
+    std::cout << "Complete.";
+}
+
+int main() {
+
+    int workMode = 0;
+    bool wantsEncrypt = 0;
+
+    std::cout << "Hello, please select work mode: " << std::endl << "    1) Generate passwords" << std::endl << "    2) Decrypt passwords" << std::endl << "Enter work mode here:";
+
+    std::cin >> workMode;
+
+    switch (workMode) {
+        case 1:
+            Generator();
+            std::cout << "If you want to encrypt the received file enter 1 otherwise enter 0: ";
+            std::cin >> wantsEncrypt;
+            if (wantsEncrypt){
+                Encryptor();
+            } else {
+                std::cout << "Thank you for using this program. Good day.";
+            }
+            break;
+        case 2:
+            Decrypter();
+            break;
+        default:
+            std::cout << "You are entered wrong number of work mode !";
+    }
 
     return 0;
 }
